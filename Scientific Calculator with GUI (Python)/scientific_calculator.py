a1ab64e8e91493c39a860c6d3576674ac2012763
sid-jOkEr <siddesh.shewde@gmail.com>
2020-09-12 03:16:37 +0530

    added scientific buttons

--- Scientific Calculator with GUI (Python)/scientific_calculator.py
+++ Scientific Calculator with GUI (Python)/scientific_calculator.py
@@ -16,12 +16,12 @@ def exit():
 # Standard Calculator Function
 def standard():
     root.resizable(width = False, height = False)
-    root.geometry("600x568+0+0")
+    root.geometry("450x300+0+0")
 
 # Scientific Calculator Function
 def scientific():
     root.resizable(width = False, height = False)
-    root.geometry("6944x568+0+0")
+    root.geometry("694x568+0+0")
 
 
 
@@ -42,32 +42,56 @@ calc.grid(ipadx=100, ipady=100)
 ### Inner Grid ###
 
 text_display = Entry(calc, font=('arial', 20, 'bold'), bg = "powder blue", bd = 30, width = 28, justify = RIGHT)
-text_display.grid(row = 0, column = 0, columnspan = 4, pady = 10)
+text_display.grid(row = 0, column = 0, columnspan = 4, pady = 0)
 text_display.insert(0, "0")
 
+# Standard Calculator
 number_pad = "1234567890"
 i = 0
 button = []
 
 for j in range (2, 5):
     for k in range (3):
-        button.append(Button(calc, width=6, height=2, font=('arial', 20, 'bold'), bd=4, text = number_pad[i]))
-        button[i].grid(row = j, column = k, pady = 1)
+        button.append(Button(calc, width=6, height=2, font=('arial', 20, 'bold'), text = number_pad[i]))
+        button[i].grid(row = j, column = k)
 
         i += 1
 
-clear_button = Button(calc, text = "C", width = 6, height = 2, font=('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 1, column = 1)
-clearAll_button = Button(calc, text = "CE", width = 6, height = 2, font=('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 1, column = 0)
-back_button = Button(calc, text = "Back", width = 6, height = 2, font=('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 1, column = 2)
-zero_button = Button(calc, text = "0", width = 6, height = 2, font=('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 5, column = 1)
-equal_button = Button(calc, text = "=", width = 13, height = 2, font=('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 5, column = 2, columnspan = 2)
+clear_button = Button(calc, text = "C", width = 6, height = 2, font=('arial', 20, 'bold'),  bg = "powder blue").grid(row = 1, column = 1)
+clearAll_button = Button(calc, text = "CE", width = 6, height = 2, font=('arial', 20, 'bold'),  bg = "powder blue").grid(row = 1, column = 0)
+back_button = Button(calc, text = "Back", width = 6, height = 2, font=('arial', 20, 'bold'),  bg = "powder blue").grid(row = 1, column = 2)
 
-add_button = Button(calc, text = "+", width = 6, height = 2, font=('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 1, column = 3)
-subtract_button = Button(calc, text = "-", width = 6, height = 2, font=('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 2, column = 3)
-multiply_button = Button(calc, text = "*", width = 6, height = 2, font=('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 3, column = 3)
-divide_button = Button(calc, text = "/", width = 6, height = 2, font=('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 4, column = 3)
-dot_button = Button(calc, text = ".", width = 6, height = 2, font=('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 5, column = 0)
+dot_button = Button(calc, text = ".", width = 6, height = 2, font=('arial', 20, 'bold'),  bg = "powder blue").grid(row = 5, column = 0)
+zero_button = Button(calc, text = "0", width = 6, height = 2, font=('arial', 20, 'bold'),  bg = "powder blue").grid(row = 5, column = 1)
+equal_button = Button(calc, text = "=", width = 13, height = 2, font=('arial', 20, 'bold'),  bg = "powder blue").grid(row = 5, column = 2, columnspan = 2)
 
+add_button = Button(calc, text = "+", width = 6, height = 2, font=('arial', 20, 'bold'),  bg = "powder blue").grid(row = 1, column = 3)
+subtract_button = Button(calc, text = "-", width = 6, height = 2, font=('arial', 20, 'bold'),  bg = "powder blue").grid(row = 2, column = 3)
+multiply_button = Button(calc, text = "*", width = 6, height = 2, font=('arial', 20, 'bold'),  bg = "powder blue").grid(row = 3, column = 3)
+divide_button = Button(calc, text = "/", width = 6, height = 2, font=('arial', 20, 'bold'),  bg = "powder blue").grid(row = 4, column = 3)
+
+# Scientific Calculator
+sin_button = Button(calc, text = "sin", width = 6, height = 2, font=('arial', 20, 'bold'), bg = "powder blue").grid(row = 1, column = 5)
+cos_button = Button(calc, text = "cos", width = 6, height = 2, font=('arial', 20, 'bold'),  bg = "powder blue").grid(row = 1, column = 6)
+tan_button = Button(calc, text = "tan", width = 6, height = 2, font=('arial', 20, 'bold'),  bg = "powder blue").grid(row = 1, column = 7)
+
+log_button = Button(calc, text = "log", width = 6, height = 2, font=('arial', 20, 'bold'),  bg = "powder blue").grid(row = 2, column = 5)
+exponent_button = Button(calc, text = "e", width = 6, height = 2, font=('arial', 20, 'bold'),  bg = "powder blue").grid(row = 2, column = 6)
+pi_button = Button(calc, text = "π", width = 6, height = 2, font=('arial', 20, 'bold'),  bg = "powder blue").grid(row = 2, column = 7)
+
+
+
+percent_button = Button(calc, text = "%", width = 6, height = 2, font=('arial', 20, 'bold'),  bg = "powder blue").grid(row = 3, column = 5)
+power_button = Button(calc, text = "^", width = 6, height = 2, font=('arial', 20, 'bold'),  bg = "powder blue").grid(row = 3, column = 6)
+factorial_button = Button(calc, text = "!", width = 6, height = 2, font=('arial', 20, 'bold'),  bg = "powder blue").grid(row = 3, column = 7)
+
+sqrt_button = Button(calc, text = "√", width = 6, height = 2, font=('arial', 20, 'bold'),  bg = "powder blue").grid(row = 4, column = 5)
+ln_button = Button(calc, text = "ln", width = 6, height = 2, font=('arial', 20, 'bold'),  bg = "powder blue").grid(row = 4, column = 6)
+inverse_button = Button(calc, text = "+/-", width = 6, height = 2, font=('arial', 20, 'bold'),  bg = "powder blue").grid(row = 4, column = 7)
+
+openbracket_button = Button(calc, text = "(", width = 6, height = 2, font=('arial', 20, 'bold'),  bg = "powder blue").grid(row = 5, column = 5)
+closebracket_button = Button(calc, text = ")", width = 6, height = 2, font=('arial', 20, 'bold'),  bg = "powder blue").grid(row = 5, column = 6)
+closebracket_button = Button(calc, text = ")", width = 6, height = 2, font=('arial', 20, 'bold'),  bg = "powder blue").grid(row = 5, column = 6)
 
 ### Inner Grid ###
 
@@ -81,8 +105,8 @@ menubar = Menu(calc)
 # File Menu
 filemenu = Menu(menubar, tearoff=0)
 menubar.add_cascade(label = "File", menu = filemenu)
-filemenu.add_command(label = "Standard")
-filemenu.add_command(label = "Scientific")
+filemenu.add_command(label = "Standard", command = standard)
+filemenu.add_command(label = "Scientific", command = scientific)
 filemenu.add_command(label = "Send Feedback")
 filemenu.add_separator()
 filemenu.add_command(label = "Exit", command = exit)
@@ -101,9 +125,6 @@ helpmenu.add_command(label = "View Help")
 
 ### Menu Bar ###
 
-
-root.grid_rowconfigure(0, weight=1)
-root.grid_columnconfigure(0, weight=1)
 
 root.config(menu = menubar)
 root.mainloop()
