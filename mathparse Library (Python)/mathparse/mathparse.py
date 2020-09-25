fb64014cc202ed58d9c4cdaf9c4a4a41f5f3d529
sid-jOkEr <siddesh.shewde@gmail.com>
2020-09-26 02:53:21 +0530

    commit

--- mathparse Library (Python)/mathparse/mathparse.py
+++ mathparse Library (Python)/mathparse/mathparse.py
@@ -1,4 +1,5 @@
 import mathnotations
+import re
 
 notations = mathnotations.word_notations()
 
@@ -10,4 +11,47 @@ def parse (expression):
 
     print (expression)
 
+
+
+def number_formation():
+    numbers = [1,1000,200,20,5]
+    if len(numbers) == 4:
+        print ((numbers[0] * numbers[1]) + numbers[2] + numbers[3])
+    elif len(numbers) == 3:
+        print (numbers[0] * numbers[1] + numbers[2])
+    elif len(numbers) == 2:
+        if 100 in numbers:
+            print ( numbers[0] * numbers[1])
+        else:
+            print (numbers[0] + numbers[1])
+    else:
+        print (numbers[0])
+
+
+def find_word_groups(string, words):
+    """
+    Find matches for words in the format "3 thousand 6 hundred 2".
+    The words parameter should be the list of words to check for
+    such as "hundred".
+    """
+    scale_pattern = '|'.join(words)
+    # For example:
+    # (?:(?:\d+)\s+(?:hundred|thousand)*\s*)+(?:\d+|hundred|thousand)+
+    print (scale_pattern)
+    regex = re.compile(
+        r'(?:(?:\d+)\s+(?:' +
+        scale_pattern +
+        r')*\s*)+(?:\d+|' +
+        scale_pattern + r')+'
+    )
+    print (regex)
+    result = regex.findall(string)
+    print (result)
+    print (string)
+    return result
+
 parse ('one hundred and twenty five')
+number_formation()
+print (find_word_groups('one hundred and twenty five', list(notations['scales'].keys())))
+
+
