6298892cfb01f1d63cbd07838794dd1fe42b2852
sid-jOkEr <siddesh.shewde@gmail.com>
2020-10-01 03:21:00 +0530

    jarvis added changelog

--- J.A.R.V.I.S AI Assistant (Python)/test-staging/test.py
+++ J.A.R.V.I.S AI Assistant (Python)/test-staging/test.py
@@ -8,13 +8,25 @@ import speech_recognition as sr
 print (sr.__version__)
 
 recognizer = sr.Recognizer()
+mic = sr.Microphone()
 
 song = sr.AudioFile('test.wav')
 
-with song as source:
-    audio = recognizer.record(source)
+with mic as source:
+    print ('with block')
+    recognizer.adjust_for_ambient_noise(source)
+    audio = recognizer.listen(source)
+
+try:
+    print ('listening..')
+    query = recognizer.recognize_google(audio)
+    print (query)
+except sr.RequestError:
+    print('api error')
+
+except sr.UnknownValueError:
+    print ('Unable to recognise speech')
 
 print(type(audio))
 
 
-recognizer.recognize_google(audio)
