a8401894fbb28ed781670841622289eb2e1fc3da
sid-jOkEr <siddesh.shewde@gmail.com>
2020-10-02 23:53:30 +0530

    jarvis test

--- J.A.R.V.I.S AI Assistant (Python)/src/start.py
+++ J.A.R.V.I.S AI Assistant (Python)/src/start.py
@@ -15,10 +15,12 @@ JARVIS_LOGO = "\n" \
 def main():
     print ('JARVIS is starting.')
     print ('performing initial checks...')
-    for i in range (3):
+    for i in range (5):
         print ('.', sep=' ', end=' ', flush=True); 
-        time.sleep(0.5)
-    # Clear Screen
+        time.sleep(1)
+    time.sleep(2)
+    clear = lambda: os.system('cls')
+    clear()
     print ('\n' + JARVIS_LOGO)    
     print ('Application started.')
     print ('Jarvis at your service. Please tell me how can I help you')
