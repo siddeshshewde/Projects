54aca4711ce03a096c3ed39ca13634c64ef6d08c
siddeshshewde <siddesh.shewde@gmail.com>
2021-10-14 18:29:58 -0400

    Added Support for Xoom
    - Xoom Exchange Rate Added
    - Commented sending emails for now (temporary disabled)

--- Exchange Rate Tracker (Python)/main.py
+++ Exchange Rate Tracker (Python)/main.py
@@ -18,6 +18,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 def main():
 
     remitly_rate = ''
+    xoom_rate = ''
 
     # Remitly - https://www.remitly.com/no/en
     url = 'https://www.remitly.com/us/en/india/pricing'
@@ -25,24 +26,33 @@ def main():
     soup = BeautifulSoup(response, 'html.parser')
 
     div_elements = soup.find_all('div', class_ = 'f1wm94yy' and 'fnsgms5')
-    remitly_rate = div_elements[0].get_text()
+    remitly_rate = '1 USD = ' + div_elements[0].get_text().replace('₹', '') + ' INR'
     print (remitly_rate)
 
+    # Xoom - https://www.xoom.com/india/send-money
+    url = 'https://www.xoom.com/india/send-money'
+    response = requests.get(url).text
+    soup = BeautifulSoup(response, 'html.parser')
 
-    my_sg = sendgrid.SendGridAPIClient(api_key = 'API Key')
+    div_elements = soup.find_all('p', class_ = 'xvx-text-right' and 'js-ui-content-update-animation-item' and 'xvx-font-copy')
+    xoom_rate = div_elements[0].get_text().replace('*', '')
+    print (xoom_rate)
 
-    from_email = Email("siddesh.shewde@gmail.com")  
-    to_email = To("shahaa62@gmail.com")  
-    subject = "Remitly Exchange Rate: " + str(date.today())
-    mail_body = "Remitly Current Rate: " + remitly_rate
-    content = Content("text/plain", mail_body)
 
-    mail = Mail(from_email, to_email, subject, content)
+    # my_sg = sendgrid.SendGridAPIClient(api_key = 'API KEY')
 
-    # Get a JSON-ready representation of the Mail object
-    mail_json = mail.get()
+    # from_email = Email("x@gmail.com")  
+    # to_email = To("x@gmail.com")  
+    # subject = "Remitly Exchange Rate: " + str(date.today())
+    # mail_body = "Remitly Current Rate: " + remitly_rate
+    # content = Content("text/plain", mail_body)
 
-    # Send an HTTP POST request to /mail/send
-    HTTP_response = my_sg.client.mail.send.post(request_body=mail_json)
+    # mail = Mail(from_email, to_email, subject, content)
+
+    # # Get a JSON-ready representation of the Mail object
+    # mail_json = mail.get()
+
+    # # Send an HTTP POST request to /mail/send
+    # HTTP_response = my_sg.client.mail.send.post(request_body=mail_json)
 
 main()
