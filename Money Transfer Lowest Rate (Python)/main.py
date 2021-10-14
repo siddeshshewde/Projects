13279af471bf925dcfa6ad47d21597ef0587c851
siddeshshewde <siddesh.shewde@gmail.com>
2021-10-14 03:09:57 -0400

    Added Email Sending through smtp

--- Money Transfer Lowest Rate (Python)/main.py
+++ Money Transfer Lowest Rate (Python)/main.py
@@ -1,8 +1,8 @@
 import requests
-import json
-import html_to_json
-from lxml import html, etree
 from bs4 import BeautifulSoup
+import sendgrid
+import os
+from sendgrid.helpers.mail import Mail, Email, To, Content
 
 def main():
     remitly_rate = ''
@@ -15,5 +15,21 @@ def main():
     div_elements = soup.find_all('div', class_ = 'f1wm94yy' and 'fnsgms5')
     remitly_rate = div_elements[0].get_text()
     print (remitly_rate)
+
+
+    my_sg = sendgrid.SendGridAPIClient(api_key = 'APIKEY')
+
+    from_email = Email("siddesh.shewde@gmail.com")  
+    to_email = To("shahaa62@gmail.com")  
+    subject = "Test Email"
+    content = Content("text/plain", "Test")
+
+    mail = Mail(from_email, to_email, subject, content)
+
+    # Get a JSON-ready representation of the Mail object
+    mail_json = mail.get()
+
+    # Send an HTTP POST request to /mail/send
+    HTTP_response = my_sg.client.mail.send.post(request_body=mail_json)
 
 main()
